199e9a1634f82c813cc19807b1c2ca5611e75828
MinaHany256 <minahanyfoad@gmail.com>
2024-10-06 22:03:17 +0300

    Part 09 Unit Of Work

---
+++ LinkDev.Talabat.Core.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using LinkDev.Talabat.Core.Domain.Entites.Products;
+
+namespace LinkDev.Talabat.Core.Domain.Contracts
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IGenericRepository<Product, int> ProductRepository { get;  }
+        public IGenericRepository<ProductBrand, int> BrandsRepository { get;  }
+        public IGenericRepository<ProductCategory, int> CategoriesRepository { get; }
+
+        Task<int> CompleteAsync();
+
+    }
+}
