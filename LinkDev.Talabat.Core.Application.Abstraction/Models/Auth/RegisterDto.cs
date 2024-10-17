db6f96225b7708c8c3a4c8e40ba1a4f06e1846a7
MinaHany256 <minahanyfoad@gmail.com>
2024-10-17 23:02:43 +0300

    Part 04 Auth Service - Login and Register

---
+++ LinkDev.Talabat.Core.Application.Abstraction/Models/Auth/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDev.Talabat.Core.Application.Abstraction.Models.Auth
+{
+    public class RegisterDto
+    {
+        [Required]
+        public required string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string UserName { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Phone { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Core.Application/Services/Auth/AuthService.cs
@@ -0,0 +1,58 @@
+﻿using LinkDev.Talabat.Core.Application.Abstraction.Models.Auth;
+using LinkDev.Talabat.Core.Application.Abstraction.Services.Auth;
+using LinkDev.Talabat.Core.Application.Exceptions;
+using LinkDev.Talabat.Core.Domain.Entites.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace LinkDev.Talabat.Core.Application.Services.Auth
+{
+    internal class AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : IAuthService
+    {
+        public async Task<UserDto> LoginAsync(LoginDto model)
+        {
+            var user = await userManager.FindByEmailAsync(model.Email);
+
+            if (user is null) throw new BadRequestException("Invalid Login");
+
+            var reult = await signInManager.CheckPasswordSignInAsync(user, model.Password, lockoutOnFailure: true);
+
+            if (!reult.Succeeded) throw new BadRequestException("Invalid Login");
+
+            var response = new UserDto()
+            {
+                Id = user.Id,
+                DisplayName = user.DisplayName,
+                Email = user.Email!,
+                Token = "This will bw JWT Token"
+            };
+
+            return response;
+        }
+
+        public async Task<UserDto> Register(RegisterDto model)
+        {
+            var user = new ApplicationUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.UserName,
+                PhoneNumber = model.Phone,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded) throw new ValidationException() { Errors = result.Errors.Select(E => E.Description)};
+
+
+            var response = new UserDto()
+            {
+                Id = user.Id,
+                DisplayName = user.DisplayName,
+                Email = user.Email!,
+                Token = "This will bw JWT Token"
+            };
+
+            return response;
+        }
+    }
+}
