7f4d7a4c953af870f489c9fe28123e7f9cd00ce2
MinaHany256 <minahanyfoad@gmail.com>
2024-10-17 16:23:44 +0300

    Part 01 Identity Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entites/Identity/Address.cs
@@ -0,0 +1,17 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+
+namespace LinkDev.Talabat.Core.Domain.Entites.Identity
+{
+    public class Address 
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Street { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+
+        public int UserId { get; set; }
+        public required ApplicationUser AppUser { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Entites/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LinkDev.Talabat.Core.Domain.Entites.Identity
+{
+    public class ApplicationUser : IdentityUser<string>
+    {
+        public required string DisplayName { get; set; }
+        public Address? Address { get; set; }
+
+    }
+}
